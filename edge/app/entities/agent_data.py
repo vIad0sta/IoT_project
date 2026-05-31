1c66d6a74cf70842a4c267f08cffed32019f9f66
Vlad <vladshepel.04@gmail.com>
2026-05-31 18:54:05 +0300

    add to edge: data structure for parking, traffic lights

--- edge/app/entities/agent_data.py
+++ edge/app/entities/agent_data.py
@@ -1,27 +1,44 @@
 from datetime import datetime
 from pydantic import BaseModel, field_validator
 
+
 class AccelerometerData(BaseModel):
     x: float
     y: float
     z: float
 
+
 class GpsData(BaseModel):
     latitude: float
     longitude: float
 
+
+class ParkingData(BaseModel):
+    timestamp:   datetime
+    gps:         GpsData
+
+class TrafficLightData(BaseModel):
+    state:          str    
+    time_remaining: int
+    timestamp:      datetime
+    gps:            GpsData
+
+
 class AgentData(BaseModel):
     accelerometer: AccelerometerData
     gps: GpsData
+    parking: ParkingData
+    traffic_light: TrafficLightData
     timestamp: datetime
 
-@classmethod
-@field_validator('timestamp', mode='before')
-def parse_timestamp(cls, value):
-# Convert the timestamp to a datetime object
-    if isinstance(value, datetime):
-        return value
-    try:
-        return datetime.fromisoformat(value)
-    except (TypeError, ValueError):
-        raise ValueError("Invalid timestamp format. Expected ISO 8601 format(YYYY-MM-DDTHH:MM:SSZ).")
+    @classmethod
+    @field_validator('timestamp', mode='before')
+    def parse_timestamp(cls, value):
+        if isinstance(value, datetime):
+            return value
+        try:
+            return datetime.fromisoformat(value)
+        except (TypeError, ValueError):
+            raise ValueError(
+                "Invalid timestamp format. Expected ISO 8601 format (YYYY-MM-DDTHH:MM:SSZ)."
+            )
--- edge/docker/db/structure.sql
+++ edge/docker/db/structure.sql
@@ -1,10 +1,26 @@
-CREATE TABLE processed_agent_data (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE road_state_data (
+    id         SERIAL PRIMARY KEY,
     road_state VARCHAR(255) NOT NULL,
-    x FLOAT,
-    y FLOAT,
-    z FLOAT,
-    latitude FLOAT,
-    longitude FLOAT,
-    timestamp TIMESTAMP
+    x          FLOAT,
+    y          FLOAT,
+    z          FLOAT,
+    latitude   FLOAT,
+    longitude  FLOAT,
+    timestamp  TIMESTAMP
+);
+
+CREATE TABLE parking_data (
+    id          SERIAL PRIMARY KEY,
+    timestamp   TIMESTAMP    NOT NULL,
+    latitude    FLOAT        NOT NULL,
+    longitude   FLOAT        NOT NULL
+);
+
+CREATE TABLE traffic_light_data (
+    id             SERIAL PRIMARY KEY,
+    timestamp      TIMESTAMP    NOT NULL,
+    latitude       FLOAT        NOT NULL,
+    longitude      FLOAT        NOT NULL,
+    state          VARCHAR(10)  NOT NULL CHECK (state IN ('red', 'yellow', 'green')),
+    time_remaining INTEGER      NOT NULL
 );
